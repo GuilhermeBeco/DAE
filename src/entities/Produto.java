3052a0e36cf274ed064c8380ddaf973af06aa688
2171632 <2171632@my.ipleiria.pt>
2019-11-21 17:34:04 +0000

    mensagem

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -19,9 +19,9 @@ public class Escalao {
     @Id
     private int id;
     @NotNull
-    private String name;
+    private String tipo;
     @NotNull
-    private int startAge;
+    private String descricao;
     @NotNull
     private int endAge;
     @ManyToMany(mappedBy = "escaloes")
@@ -36,46 +36,6 @@ public class Escalao {
         this.name = name;
         this.startAge = startAge;
         this.endAge = endAge;
-        this.modalidades= new LinkedHashSet<Modalidade>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStartAge() {
-        return startAge;
-    }
-
-    public void setStartAge(int startAge) {
-        this.startAge = startAge;
-    }
-
-    public int getEndAge() {
-        return endAge;
-    }
-
-    public void setEndAge(int endAge) {
-        this.endAge = endAge;
-    }
-
-    public Collection<Modalidade> getModalidades() {
-        return modalidades;
-    }
-
-    public void addModalidade(Modalidade modalidade){
-        modalidades.add(modalidade);
+        this.modalidades=modalidades;
     }
 }
